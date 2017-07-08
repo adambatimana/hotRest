d3563fa3a9ab179ef12354aa52797b2fdd1c237c
Adam Batimana <adambatimana@gmail.com>
2017-07-08 11:00:04 -0400

    init files

---
+++ server.js
@@ -0,0 +1,108 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+  type: "application/vnd.api+json"
+}));
+
+// ====================================
+//             jQuery
+// ====================================
+
+//create object from jquery to PUSH INTO object arrays
+
+//GET information from frontend to put into array for tables
+let tables = [{}];
+
+
+//GET information from frontend to put into array for the waitlist
+let waitList = [{}];
+
+
+// ====================================
+//             ROUTING
+// ====================================
+app.get("/", function(req, res) {
+  res.send("THIS IS THE HOMEPAGE!")
+  // res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/tables", function(req, res) {
+  res.send("THIS IS THE TABLES PAGE!")
+
+  // res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/reserve", function(req, res) {
+  res.send("THIS IS THE Reservation page!")
+
+  // res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// app.get("/api/tables", function(req, res) {
+//   res.json();
+// });
+
+// app.get("/api/waitlist", function(req, res) {
+//   res.json();
+// });
+
+
+// ====================================
+//             GET REQUEST
+// ====================================
+
+// app.get("/api/:characters?", function(req, res) {
+//   var chosen = req.params.characters;
+//
+//   return res.json();
+// });
+
+
+// ====================================
+//            POST REQUEST
+// ====================================
+
+// app.post("/api/new", function(req, res) {
+//
+//   var newTable = req.body;
+//
+//   console.log(newTable);
+//
+//   "table".push();
+//
+//
+//   res.json();
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
